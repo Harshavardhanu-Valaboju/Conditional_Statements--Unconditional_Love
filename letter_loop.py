87802889aeab33577ab7a4d91498f3eaf51f5890
Harsha Valaboju <harshavardhanuvalaboju91@gmail.com>
2026-06-08 19:54:35 +0530

    Added a cute python project

---
+++ letter_loop.py
@@ -0,0 +1,14 @@
+print("If you want surprise please enter open to get the surprise")
+a=input()
+if a.lower() =="open":
+    print("Hello Beautiful")
+    print("Choose yes or no for the upcoming question")
+    print ()
+    print("Do you love me?")
+    b=input("Type answer here :")
+    while b.lower() !="yes":
+        print("heyy you are lying, give geniune answer")
+        b=input("Enter again :")
+    print("Yes You are right I Too Love You My Sweetheart (: ")
+else:
+    print("Invalid input!! Please run the code again and enter correct input")
